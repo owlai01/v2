fb8a178bbec4081aea64b2d6f531eb2cd0d410ac
owlai01 <152986905+owlai01@users.noreply.github.com>
2024-09-01 06:55:51 +0300

    Add files via upload

--- Cmds/General/ping.js
+++ Cmds/General/ping.js
@@ -2,6 +2,6 @@ module.exports = async (context) => {
         const { client, m, dreadedspeed } = context;
 
 
-await m.reply(`Pong\n${dreadedspeed.toFixed(4)}ms`)
+await m.reply(`Latency\n${dreadedspeed.toFixed(4)}ms`)
 
 }
